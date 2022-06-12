4afa890ac54e6071f132c4482af377ce001934cb
krhistopher <krhiskk@gmail.com>
2022-06-11 21:42:13 -0300

    first commit

---
+++ aula11/script.js
@@ -0,0 +1,8 @@
+alert('Com a nossa mensagem')
+const num1 = Number(prompt("Digite o primeiro numero: "))
+const num2 = Number(prompt("Digite o segundo numero: "))
+
+
+resultado = num1 + num2
+
+alert(`O resultado foi ${resultado}`)
