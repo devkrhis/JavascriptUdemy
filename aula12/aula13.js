fd91121dff1561fb0906fd9073b601a09543a883
krhistopher <krhiskk@gmail.com>
2022-06-17 18:10:58 -0300

    ok sobe

---
+++ aula12/aula13.js
@@ -0,0 +1,7 @@
+let umaString = "Um texto";
+
+console.log(umaString.charAt(4)) // charAt é uma função para eu pegar a posição do indice.
+console.log(umaString.concat('', 'em', 'um', '', 'lindo', '', '','dia')) // concat ele concatena tudo, sendo assim ele vai juntar tudo
+console.log(umaString.indexOf("texto")) // vai me mostrar em qual indice está a palavra texto
+console.log(umaString.replace("Um", "vai virar krhis lindão hahaha")) // aqui eu substituo a palavra para que eu quero
+console.log(umaString.length) // aqui eu sei o tamanho da string.
